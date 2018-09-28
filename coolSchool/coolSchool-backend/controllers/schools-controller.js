9a4ac3b849e4303cbd850f260dea4af441b40f79
t-dinh <tdinh104@gmail.com>
2018-09-28 15:01:16 -0700

    fixed call error

--- coolSchool/coolSchool-backend/controllers/schools-controller.js
+++ coolSchool/coolSchool-backend/controllers/schools-controller.js
@@ -1,10 +1,6 @@
 const schoolData  = require('../data/users');
 
-const helper = (name, searching, schoolData) => {
-    let item = schoolData.filter(p => p['name'] == name).map(p => p[searching]).pop();
-    let obj = { ['name']: name, [searching]: item }
-    return obj;
-}
+
 const index = (req, res) => {
     res.json({ data: schoolData });
 }
@@ -12,13 +8,14 @@ const index = (req, res) => {
 const rank = (req, res) => {
     let { rank } = req.query;
     console.log(req.query)
-    let schoolRank = schoolData.filter(s => s.rank == rank ).pop();
+    let schoolRank = schoolData.filter(s => s.rank == rank );
     // let schoolList = schoolData.map(s => s.schoolList)
     console.log(schoolRank)
     if (schoolRank === undefined) {
         res.send({ message:'cannot find' })
     } else ( res.send({ schoolRank }))
 }
+
 
 
 
